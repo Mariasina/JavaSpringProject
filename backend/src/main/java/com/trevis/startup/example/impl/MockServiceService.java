5c37e831a7712680f95da2e2d05bfa73392fab09
Mariasina <carolbeveer@gmail.com>
2024-06-19 15:49:16 -0300

    service mock done

--- backend/src/main/java/com/trevis/startup/example/impl/MockServiceService.java
+++ backend/src/main/java/com/trevis/startup/example/impl/MockServiceService.java
@@ -1,9 +1,32 @@
 package com.trevis.startup.example.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.trevis.startup.example.model.Service;
 import com.trevis.startup.example.services.ServiceService;
 
 public class MockServiceService implements ServiceService {
+    private List<Service> services =  new ArrayList<>();
+
     @Override
-    public void get(String query, int pageIndex, int pageSize) {
+    public List<Service> get(String query, int pageIndex, int pageSize) {
+        if(query != null){
+            pageIndex *= pageSize;
+            var requiredServices = services.stream().filter(u -> u.getName().contains(query)).toList();
+
+            List<Service> pageServices =  new ArrayList<>();
+            for (int i = pageIndex - pageSize; i < pageIndex; i++) {
+                pageServices.add(requiredServices.get(i));
+            }
+            return pageServices;
+        }
+        else{
+            List<Service> pageServices =  new ArrayList<>();
+            for (int i = pageIndex - pageSize; i < pageIndex; i++) {
+                pageServices.add(services.get(i));
+            }
+            return pageServices;
+        }
     }
 }
--- backend/src/main/java/com/trevis/startup/example/services/ServiceService.java
+++ backend/src/main/java/com/trevis/startup/example/services/ServiceService.java
@@ -1,5 +1,9 @@
 package com.trevis.startup.example.services;
 
+import java.util.List;
+
+import com.trevis.startup.example.model.Service;
+
 public interface ServiceService {
-    void get(String query, int pageIndex, int pageSize);
+    List<Service> get(String query, int pageIndex, int pageSize);
 }
