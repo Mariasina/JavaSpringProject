c45a7584e23c6d928a9fd297de7e5103ab471a1a
JoviOakz <joao.mateussdc@gmail.com>
2024-06-19 14:11:04 -0300

    feat: MockServiceService

---
+++ backend/src/main/java/com/trevis/startup/example/impl/MockServiceService.java
@@ -0,0 +1,9 @@
+package com.trevis.startup.example.impl;
+
+import com.trevis.startup.example.services.ServiceService;
+
+public class MockServiceService implements ServiceService {
+    @Override
+    public void get(String query, int pageIndex, int pageSize) {
+    }
+}
