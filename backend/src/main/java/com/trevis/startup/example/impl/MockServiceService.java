5d1babcd63d204e6e06703cbdaa5eab35f019d42
Mariasina <carolbeveer@gmail.com>
2024-06-19 15:53:13 -0300

    added services to mock

--- backend/src/main/java/com/trevis/startup/example/impl/MockServiceService.java
+++ backend/src/main/java/com/trevis/startup/example/impl/MockServiceService.java
@@ -3,11 +3,24 @@ package com.trevis.startup.example.impl;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.trevis.startup.example.model.Department;
 import com.trevis.startup.example.model.Service;
 import com.trevis.startup.example.services.ServiceService;
 
 public class MockServiceService implements ServiceService {
     private List<Service> services =  new ArrayList<>();
+
+    public MockServiceService() {
+        var newService = new Service();
+        newService.setId(1l);
+        newService.setName("Sites legais");
+        services.add(newService);
+
+        newService = new Service();
+        newService.setId(2l);
+        newService.setName("TI doida");
+        services.add(newService);
+    }
 
     @Override
     public List<Service> get(String query, int pageIndex, int pageSize) {
