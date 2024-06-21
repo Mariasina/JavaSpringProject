97d2ae836d0ec80d66aa22362758f98220b2708a
JoviOakz <joao.mateussdc@gmail.com>
2024-06-21 16:15:32 -0300

    fix: DatabaseUserService

--- backend/src/main/java/com/trevis/startup/example/impl/DatabaseUserService.java
+++ backend/src/main/java/com/trevis/startup/example/impl/DatabaseUserService.java
@@ -1,5 +1,74 @@
 package com.trevis.startup.example.impl;
 
-public class DatabaseUserService {
-    
+import java.util.Random;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.trevis.startup.example.model.DepartmentData;
+import com.trevis.startup.example.model.UserData;
+import com.trevis.startup.example.repositories.UserJPARepository;
+import com.trevis.startup.example.services.PasswordService;
+import com.trevis.startup.example.services.UserService;
+
+public class DatabaseUserService implements UserService {
+
+    @Autowired
+    UserJPARepository repo;
+
+    @Autowired
+    PasswordService passService;
+
+    @Override
+    public Boolean create(Long id, String username, Integer role, DepartmentData department) {
+        var user = new UserData();
+        user.setId(id);
+        user.setUsername(username);
+        user.setRole(role);
+        user.setDepartment(department);
+
+        Random random = new Random();
+
+        String[] characters = { "0", "1", "b", "2", "4", "5", "6", "7", "8",
+                "9", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
+                "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w",
+                "x", "y", "z" };
+
+        String password = "";
+
+        for (int i = 0; i < 8; i++) {
+            int position = random.nextInt(0, 36);
+            password += characters[position];
+        }
+
+        String encryptedPassword;
+        encryptedPassword = passService.applyCryptography(password);
+        user.setPassword(encryptedPassword);
+
+        repo.save(user);
+
+        return true;
+    }
+
+    @Override
+    public Boolean updatePassword(Long id, String newPassword) {
+        UserData loginUser = repo.findById(id).orElse(null);
+
+        if (loginUser == null) {
+            return false;
+        }
+
+        if (!passService.verifyRules(newPassword)) {
+            return false;
+        }
+
+        String encryptedPassword = passService.applyCryptography(newPassword);
+        loginUser.setPassword(encryptedPassword);
+
+        return true;
+    }
+
+    @Override
+    public UserData get(String username) {
+        return repo.findByUsername(username);
+    }
 }
--- backend/src/main/java/com/trevis/startup/example/repositories/UserJPARepository.java
+++ backend/src/main/java/com/trevis/startup/example/repositories/UserJPARepository.java
@@ -1,7 +1,5 @@
 package com.trevis.startup.example.repositories;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +7,5 @@ import com.trevis.startup.example.model.UserData;
 
 @Repository
 public interface UserJPARepository extends JpaRepository<UserData, Long>{
-    List<UserData> findByUsername(String username);
+    UserData findByUsername(String username);
 }
