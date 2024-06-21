d3e1a05f18c3538ae51652f271aa6494ddb23eea
Mariasina <carolbeveer@gmail.com>
2024-06-21 14:01:00 -0300

    created user and service repositories

--- backend/src/main/java/com/trevis/startup/example/repositories/ServiceJPARepository.java
+++ backend/src/main/java/com/trevis/startup/example/repositories/ServiceJPARepository.java
@@ -1,5 +1,11 @@
 package com.trevis.startup.example.repositories;
 
-public class ServiceJPARepository {
-    
+import java.security.Provider.Service;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+
+public interface ServiceJPARepository extends JpaRepository<Service, Long>{
+    List<Service> findByNameContaining(String name);
 }
--- backend/src/main/java/com/trevis/startup/example/repositories/UserJPARepository.java
+++ backend/src/main/java/com/trevis/startup/example/repositories/UserJPARepository.java
@@ -1,5 +1,13 @@
 package com.trevis.startup.example.repositories;
 
-public class UserJPARepository {
-    
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.trevis.startup.example.model.UserData;
+
+@Repository
+public interface UserJPARepository extends JpaRepository<UserData, Long>{
+    List<UserData> findByUsername(String username);
 }
