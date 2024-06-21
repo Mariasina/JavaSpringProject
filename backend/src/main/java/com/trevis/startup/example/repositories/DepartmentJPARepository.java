7cc0c23fe35fe37518e982ed0192fbcaab54bfac
JoviOakz <joao.mateussdc@gmail.com>
2024-06-21 13:56:50 -0300

    feat: DepartmentJPARepository

--- backend/src/main/java/com/trevis/startup/example/repositories/DepartmentJPARepository.java
+++ backend/src/main/java/com/trevis/startup/example/repositories/DepartmentJPARepository.java
@@ -1,5 +1,15 @@
 package com.trevis.startup.example.repositories;
 
-public class DepartmentJPARepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.trevis.startup.example.model.DepartmentData;
+
+@Repository
+public interface DepartmentJPARepository extends JpaRepository<DepartmentData, Long> {
     
+    @SuppressWarnings("null")
+    List<DepartmentData> findAll();
 }
