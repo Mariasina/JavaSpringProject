07e75fed32d0e1228227bfcd514822ffc40cbab5
JoviOakz <joao.mateussdc@gmail.com>
2024-06-21 13:38:26 -0300

    fix: SuppressWarnings

--- backend/src/main/java/com/trevis/startup/example/CorsConfiguration.java
+++ backend/src/main/java/com/trevis/startup/example/CorsConfiguration.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfiguration implements WebMvcConfigurer {
 
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
         registry.addMapping("/**")
             .allowedOrigins("null")
             .allowedMethods(
