e8e25d640520a2dc541d9a11f2e2201cf56e5a47
JoviOakz <joao.mateussdc@gmail.com>
2024-06-21 16:41:58 -0300

    feat: starting controllers

---
+++ backend/src/main/java/com/trevis/startup/example/controller/ServiceServiceController.java
@@ -0,0 +1,5 @@
+package com.trevis.startup.example.controller;
+
+public class ServiceServiceController {
+    
+}
---
+++ backend/src/main/java/com/trevis/startup/example/controller/UserServiceController.java
@@ -0,0 +1,30 @@
+package com.trevis.startup.example.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.trevis.startup.example.services.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class UserServiceController {
+    
+    @Autowired
+    UserService validator;
+
+    // @PostMapping("/auth")
+    // public String postMethodName(@RequestParam String login, @RequestParam String password) {
+        
+    //     return "a";
+    // }
+
+    @PostMapping("/user")
+    public String postMethodName(@RequestParam String login, @RequestParam String department, @RequestParam String role) {
+        
+        return "a";
+    }
+    
+    
+}
