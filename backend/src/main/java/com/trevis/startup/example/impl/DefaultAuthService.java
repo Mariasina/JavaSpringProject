606bb056b8a11c067dfde0ae9d4262b12f79360a
JoviOakz <joao.mateussdc@gmail.com>
2024-06-24 14:11:36 -0300

    feat: jwt implementation

--- backend/src/main/java/com/trevis/startup/example/impl/DefaultAuthService.java
+++ backend/src/main/java/com/trevis/startup/example/impl/DefaultAuthService.java
@@ -1,25 +1,12 @@
 package com.trevis.startup.example.impl;
 
-import java.time.Instant;
-
-import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.trevis.startup.example.dto.AuthToken;
+import com.trevis.startup.example.services.AuthService;
 
-public class DefaultAuthService {
-
-    // Dados fictícios do usuário
-    String username = "john.doe";
-    String role = "admin";
-    
-    // Criação do JWT
-    String jwtToken = JWT.create()
-        .withClaim("username", username)
-        .withClaim("role", role)
-        .withClaim("createdAt", Instant.now().toEpochMilli())
-        .sign(Algorithm.HMAC256("secret")); // Assinatura com chave secreta (exemplo: "secret")
-
-    public void print() {
-        // Exibindo o token gerado
-        System.out.println("JWT Token: " + jwtToken);
+public class DefaultAuthService implements AuthService {
+    @Override
+    public AuthToken login(String username, String password) {
+        Algorithm algorithm = Algorithm.HMAC256("bungas");
     }
 }
