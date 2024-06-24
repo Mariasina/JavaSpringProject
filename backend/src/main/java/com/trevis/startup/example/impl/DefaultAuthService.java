364fe21cb4fdcbb93453320bbbb1c3d7f31810b5
JoviOakz <joao.mateussdc@gmail.com>
2024-06-24 13:55:57 -0300

    feat: jwt implementation

--- backend/src/main/java/com/trevis/startup/example/impl/DefaultAuthService.java
+++ backend/src/main/java/com/trevis/startup/example/impl/DefaultAuthService.java
@@ -1,5 +1,25 @@
 package com.trevis.startup.example.impl;
 
+import java.time.Instant;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+
 public class DefaultAuthService {
+
+    // Dados fictícios do usuário
+    String username = "john.doe";
+    String role = "admin";
     
+    // Criação do JWT
+    String jwtToken = JWT.create()
+        .withClaim("username", username)
+        .withClaim("role", role)
+        .withClaim("createdAt", Instant.now().toEpochMilli())
+        .sign(Algorithm.HMAC256("secret")); // Assinatura com chave secreta (exemplo: "secret")
+
+    public void print() {
+        // Exibindo o token gerado
+        System.out.println("JWT Token: " + jwtToken);
+    }
 }
