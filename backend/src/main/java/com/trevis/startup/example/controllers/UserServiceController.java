b38c004b475d5de4d3be15f35eab96f70921d1d2
JoviOakz <joao.mateussdc@gmail.com>
2024-06-24 16:12:30 -0300

    fix: User controller

--- backend/src/main/java/com/trevis/startup/example/controllers/UserServiceController.java
+++ backend/src/main/java/com/trevis/startup/example/controllers/UserServiceController.java
@@ -5,9 +5,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 // import com.trevis.startup.example.dto.Login;
 import com.trevis.startup.example.services.UserService;
-import org.springframework.web.bind.annotation.PostMapping;
+
+// import jakarta.websocket.server.PathParam;
+
+// import org.springframework.web.bind.annotation.PatchMapping;
+// import org.springframework.web.bind.annotation.PostMapping;
 // import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+// import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
@@ -21,10 +25,16 @@ public class UserServiceController {
     //     return 
     // }
 
-    @PostMapping("/user")
-    public String postMethodName(@RequestParam String login, @RequestParam String department, @RequestParam String role) {
+    // @PostMapping("/user")
+    // public String postMethodName(@RequestParam String login, @RequestParam String department, @RequestParam String role) {
         
-        return "a";
-    }
-    
+    //     return "a";
+    // }
+
+    // @PatchMapping("/user/{id}")
+    // public String postMethodName(@PathParam("id") int id) {
+
+    //     return "a";
+    // }
+
 }
