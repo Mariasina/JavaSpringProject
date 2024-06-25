938930a89602f3797a5516aba70eeb302f47ddf6
JoviOakz <joao.mateussdc@gmail.com>
2024-06-25 14:18:01 -0300

    a

--- backend/src/main/java/com/trevis/startup/example/controllers/UserServiceController.java
+++ backend/src/main/java/com/trevis/startup/example/controllers/UserServiceController.java
@@ -5,8 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.trevis.startup.example.dto.PasswordPayLoad;
 import com.trevis.startup.example.dto.UserCreate;
-// import com.trevis.startup.example.services.PasswordService;
-// import com.trevis.startup.example.dto.Login;
 import com.trevis.startup.example.services.UserService;
 
 import jakarta.websocket.server.PathParam;
@@ -14,7 +12,6 @@ import jakarta.websocket.server.PathParam;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-// import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
