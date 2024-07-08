b70813cb0cf652ab5cfd27e7654ada6f6602b5e0
Mariasina <carolbeveer@gmail.com>
2024-07-08 14:14:26 -0300

    working on add user integration

--- frontend/app.js
+++ frontend/app.js
@@ -1,2 +1,17 @@
 import 'regenerator-runtime/runtime';
 import axios from 'axios';
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', async event => {
+    axios.post('/user', {
+        firstName: 'Santos',
+        lastName: 'Dumont'
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+});
