4ea23d52e84aa2f3ded670b003939e5b1319f559
Mariasina <carolbeveer@gmail.com>
2024-07-08 15:22:41 -0300

    working on user endpoint

--- frontend/app.js
+++ frontend/app.js
@@ -1,17 +1,30 @@
-import 'regenerator-runtime/runtime';
-import axios from 'axios';
+// import 'regenerator-runtime/runtime';
+// import axios from 'axios';
 
-const form = document.querySelector('form');
+// const form = document.querySelector('form');
+// const login_data = document.getElementById('login').value;
+// const deparment_data = document.getElementById('department').value;
+// const role_data = document.getElementById('role').value;
 
-form.addEventListener('submit', async event => {
-    axios.post('/user', {
-        firstName: 'Santos',
-        lastName: 'Dumont'
-      })
-      .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
-});
+// const URL = "http://localhost:8080";
+
+// form.addEventListener('submit', async event => {
+//     registerUser({
+//         login : login_data,
+//         department : '2',
+//         role : '1'
+//     })
+// });
+
+// function registerUser (body) {
+//     const options = {
+//         method: 'POST',
+//         body: Object.keys(body)
+//         .map(k => `${encodeURIComponent(k)}=${encodeURIComponent(body[k])}`)
+//         .join('&')
+//     }
+//     .then(() => console.log('cadastrado'))
+
+//     return fetch(URL + "/user", options)
+//       .then(T => T.json())
+//   }
